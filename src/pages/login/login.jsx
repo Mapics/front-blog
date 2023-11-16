9eeda3d2a7a18ed8f8739998d0d60da69c2ef5bc
Mapics <nadjarmatt@gmail.com>
2023-11-16 11:14:15 +0100

    newpush

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -14,60 +14,6 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-<<<<<<< Updated upstream
-  const connexionUser = async (e) => {
-    e.preventDefault();
-
-    try {
-      const reponse = await axios.post("http://localhost:8000/login", {
-        email: email,
-        password: password,
-      });
-
-      if (reponse.data) {
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return (
-    <div className="login">
-      <div className="containerLogin">
-        <form onSubmit={connexionUser}>
-          <h2 className="title">Login</h2>
-          <p>Connectez-vous à votre compte</p>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Entrez votre email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <label htmlFor="password">Mot de passe</label>
-          <div className="containerPassword">
-            <input
-              type={showPassword ? "text" : "password"}
-              name="password"
-              id="password"
-              placeholder="Entrez votre mot de passe"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <button className="passwordVisibility" onClick={handleShowPassword}></button>
-          </div>
-          <button type="submit">Se connecter</button>
-        </form>
-        <div className="notHaveAccount">
-          <p>Vous n'avez pas de compte ?</p>
-          <Link to="/register">Inscrivez-vous</Link>
-        </div>
-      </div>
-=======
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
@@ -97,7 +43,7 @@ export default function Login() {
   }
     return (
     <div className="login">
-      <form onSubmit="handleLogin">
+      <form onSubmit={handleLogin}>
         <h2 className="title">Login</h2>
         <p>Connectez-vous à votre compte</p>
         <label htmlFor="email">Email</label>
@@ -122,7 +68,6 @@ export default function Login() {
         />
         <button type="submit">Connexion</button>
       </form>
->>>>>>> Stashed changes
     </div>
   );
 }
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,42 +1,3 @@
-<<<<<<< Updated upstream
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "./register.scss";
-
-import axios from "axios";
-
-export default function Register() {
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const [formRegister, setFormRegister] = useState({
-    name: "",
-    firstname: "",
-    email: "",
-    password: "",
-  });
-
-  const formChange = (e) => {
-    setFormRegister({ ...formRegister, [e.target.name]: e.target.value });
-  };
-
-  const addUser = async (e) => {
-    e.preventDefault();
-
-    try {
-      await axios.post("http://localhost:8000/register", formRegister);
-      setFormRegister({
-        name: "",
-        firstname: "",
-        email: "",
-        password: "",
-      });
-    } catch (error) {
-      console.log(error);
-=======
 import React, { useState } from 'react'
 import './register.scss'
 import { useNavigate } from 'react-router-dom';
@@ -67,77 +28,17 @@ export default function Register() {
 
       if (response.ok) {
         console.log('Inscription réussie');
-        navigate.push('/login');  // Redirige vers la page de connexion après l'inscription
+        navigate('/login');  // Redirige vers la page de connexion après l'inscription
       } else {
         console.error('Erreur lors de l\'inscription');
       }
     } catch (error) {
       console.error('Erreur inattendue', error);
->>>>>>> Stashed changes
     }
   };
 
   return (
     <div className="register">
-<<<<<<< Updated upstream
-      <div className="containerRegister">
-        <form onSubmit={addUser}>
-          <h2 className="title">Inscription</h2>
-          <p>Créez votre compte</p>
-          <label htmlFor="name">Nom</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Entrez votre nom"
-            value={formRegister.name}
-            onChange={formChange}
-            required
-          />
-          <label htmlFor="firstname">Prénom</label>
-          <input
-            type="text"
-            name="firstname"
-            id="firstname"
-            placeholder="Entrez votre prénom"
-            value={formRegister.firstname}
-            onChange={formChange}
-            required
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Entrez votre email"
-            value={formRegister.email}
-            onChange={formChange}
-            required
-          />
-          <label htmlFor="password">Mot de passe</label>
-          <div className="containerPassword">
-            <input
-              type={showPassword ? 'text' : 'password'}
-              name="password"
-              id="password"
-              placeholder="Entrez votre mot de passe"
-              value={formRegister.password}
-              onChange={formChange}
-              required
-            />
-            <button className="passwordVisibility" onClick={handleShowPassword}></button>
-          </div>
-          <button type="submit">Continuer</button>
-        </form>
-        <div className="haveAccount">
-          <p>Déjà un compte ?</p>
-          <Link to="/login">Connectez-vous</Link>
-        </div>
-      </div>
-    </div>
-  );
-}
-=======
       <form onSubmit={handleSignup}>
         <h2 className="title">Register</h2>
         <p>Créez votre compte</p>
@@ -186,4 +87,3 @@ export default function Register() {
     </div>
   );
 }
->>>>>>> Stashed changes
