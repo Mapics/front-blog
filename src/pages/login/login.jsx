2a65df68ab2d87ce52bc0069e1a1545543680622
M4XAW <m.picault@ecole-ipssi.net>
2023-11-16 11:13:25 +0100

    maj front

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./login.scss";
 
-import axios from "axios";
-
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
@@ -14,27 +12,36 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-<<<<<<< Updated upstream
-  const connexionUser = async (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-
     try {
-      const reponse = await axios.post("http://localhost:8000/login", {
-        email: email,
-        password: password,
+      const reponse = await fetch("http://localhost:8000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
       });
 
-      if (reponse.data) {
+      if (reponse.ok) {
+        console.log("Connexion réussie");
+      } else {
+        console.error("Erreur lors de la connexion");
       }
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.error("Erreur inattendue", err);
+    } finally {
+      setEmail("");
+      setPassword("");
     }
   };
-
   return (
     <div className="login">
       <div className="containerLogin">
-        <form onSubmit={connexionUser}>
+        <form onSubmit={handleLogin}>
           <h2 className="title">Login</h2>
           <p>Connectez-vous à votre compte</p>
           <label htmlFor="email">Email</label>
@@ -58,7 +65,10 @@ export default function Login() {
               onChange={(e) => setPassword(e.target.value)}
               required
             />
-            <button className="passwordVisibility" onClick={handleShowPassword}></button>
+            <button
+              className="passwordVisibility"
+              onClick={handleShowPassword}
+            ></button>
           </div>
           <button type="submit">Se connecter</button>
         </form>
@@ -67,62 +77,6 @@ export default function Login() {
           <Link to="/register">Inscrivez-vous</Link>
         </div>
       </div>
-=======
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const reponse = await fetch('http://localhost:8000/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        }),
-      });
-
-      if (reponse.ok) {
-        console.log('Connexion réussie');
-      } else {
-        console.error('Erreur lors de la connexion');
-      }
-
-    } catch(err) {
-      console.error('Erreur inattendue', err);
-    } finally {
-      setEmail('');
-      setPassword('');
-    }
-  }
-    return (
-    <div className="login">
-      <form onSubmit="handleLogin">
-        <h2 className="title">Login</h2>
-        <p>Connectez-vous à votre compte</p>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Entrez votre email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <label htmlFor="password">Mot de passe</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Entrez votre mot de passe"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Connexion</button>
-      </form>
->>>>>>> Stashed changes
     </div>
   );
 }
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,9 +1,7 @@
-<<<<<<< Updated upstream
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./register.scss";
-
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
   const [showPassword, setShowPassword] = useState(false);
@@ -12,36 +10,6 @@ export default function Register() {
     setShowPassword(!showPassword);
   };
 
-  const [formRegister, setFormRegister] = useState({
-    name: "",
-    firstname: "",
-    email: "",
-    password: "",
-  });
-
-  const formChange = (e) => {
-    setFormRegister({ ...formRegister, [e.target.name]: e.target.value });
-  };
-
-  const addUser = async (e) => {
-    e.preventDefault();
-
-    try {
-      await axios.post("http://localhost:8000/register", formRegister);
-      setFormRegister({
-        name: "",
-        firstname: "",
-        email: "",
-        password: "",
-      });
-    } catch (error) {
-      console.log(error);
-=======
-import React, { useState } from 'react'
-import './register.scss'
-import { useNavigate } from 'react-router-dom';
-
-export default function Register() {
   const [username, setUsername] = useState("");
   const [userfirstname, setUserfirstname] = useState("");
   const [email, setEmail] = useState("");
@@ -51,11 +19,11 @@ export default function Register() {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      console.log("ca passe")
-      const response = await fetch('http://localhost:8000/signup', {
-        method: 'POST',
+      console.log("ca passe");
+      const response = await fetch("http://localhost:8000/signup", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           email: email,
@@ -66,22 +34,20 @@ export default function Register() {
       });
 
       if (response.ok) {
-        console.log('Inscription réussie');
-        navigate.push('/login');  // Redirige vers la page de connexion après l'inscription
+        console.log("Inscription réussie");
+        navigate.push("/login"); // Redirige vers la page de connexion après l'inscription
       } else {
-        console.error('Erreur lors de l\'inscription');
+        console.error("Erreur lors de l'inscription");
       }
     } catch (error) {
-      console.error('Erreur inattendue', error);
->>>>>>> Stashed changes
+      console.error("Erreur inattendue", error);
     }
   };
 
   return (
     <div className="register">
-<<<<<<< Updated upstream
       <div className="containerRegister">
-        <form onSubmit={addUser}>
+        <form onSubmit={handleSignup}>
           <h2 className="title">Inscription</h2>
           <p>Créez votre compte</p>
           <label htmlFor="name">Nom</label>
@@ -90,8 +56,8 @@ export default function Register() {
             name="name"
             id="name"
             placeholder="Entrez votre nom"
-            value={formRegister.name}
-            onChange={formChange}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             required
           />
           <label htmlFor="firstname">Prénom</label>
@@ -100,8 +66,8 @@ export default function Register() {
             name="firstname"
             id="firstname"
             placeholder="Entrez votre prénom"
-            value={formRegister.firstname}
-            onChange={formChange}
+            value={userfirstname}
+            onChange={(e) => setUserfirstname(e.target.value)}
             required
           />
           <label htmlFor="email">Email</label>
@@ -110,22 +76,25 @@ export default function Register() {
             name="email"
             id="email"
             placeholder="Entrez votre email"
-            value={formRegister.email}
-            onChange={formChange}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
           <label htmlFor="password">Mot de passe</label>
           <div className="containerPassword">
             <input
-              type={showPassword ? 'text' : 'password'}
+              type={showPassword ? "text" : "password"}
               name="password"
               id="password"
               placeholder="Entrez votre mot de passe"
-              value={formRegister.password}
-              onChange={formChange}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               required
             />
-            <button className="passwordVisibility" onClick={handleShowPassword}></button>
+            <button
+              className="passwordVisibility"
+              onClick={handleShowPassword}
+            ></button>
           </div>
           <button type="submit">Continuer</button>
         </form>
@@ -137,53 +106,3 @@ export default function Register() {
     </div>
   );
 }
-=======
-      <form onSubmit={handleSignup}>
-        <h2 className="title">Register</h2>
-        <p>Créez votre compte</p>
-        <label htmlFor="name">Nom</label>
-        <input
-          type="text"
-          name="name"
-          id="username"
-          placeholder="Entrez votre nom"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <label htmlFor="firstname">Prénom</label>
-        <input
-          type="text"
-          name="firstname"
-          id="firstname"
-          placeholder="Entrez votre prénom"
-          value={userfirstname}
-          onChange={(e) => setUserfirstname(e.target.value)}
-          required
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Entrez votre email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <label htmlFor="password">Mot de passe</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Entrez votre mot de passe"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Inscription</button>
-      </form>
-    </div>
-  );
-}
->>>>>>> Stashed changes
